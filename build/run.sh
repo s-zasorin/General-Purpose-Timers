50a72195444980cd6176c7c871738b12d9359377
s-zasorin <ctepanzasorin@yandex.ru>
2025-11-09 13:26:19 +0300

    feat: add tb environment

---
+++ build/run.sh
@@ -0,0 +1,10 @@
+# Директория скрипта.
+
+curdir=$(pwd)
+
+# Директория артефактов симулятора.
+
+mkdir -p ${curdir}/work
+
+xrun -r tb_gpt_opt -64bit -xmlibdirpath ${curdir}/work $1 \
+    -seed $RANDOM -l ${curdir}/run.log -input xlm.tcl -input @"run"
