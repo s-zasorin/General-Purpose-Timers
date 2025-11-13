17b0ebc18a03d37b51a153dd751a5223967fab8f
Stepan Zasorin <zasorinctepan@yandex.ru>
2025-11-13 18:27:34 +0300

    fix(rtl): change signal's name in axil_if

--- rtl/regblock/axi_lite_if.sv
+++ rtl/regblock/axi_lite_if.sv
@@ -6,70 +6,40 @@ interface axi4lite_intf #(
 localparam STRB_WIDTH = (DATA_WIDTH + 7) / 8;
 
 // read address channel
-logic [ ADDR_WIDTH-1:0 ]  ar_addr;
+logic [ ADDR_WIDTH-1:0 ]  ARADDR;
 logic [            3:0 ]  ar_qos;
-logic                     ar_valid;
-logic                     ar_ready;
+logic                     ARVALID;
+logic                     ARREADY;
 
 // read data channel
-logic [ DATA_WIDTH-1:0 ]  r_data;
+logic [ DATA_WIDTH-1:0 ]  RDATA;
 logic [ RESP_WIDTH-1:0 ]  r_resp;
-logic                     r_valid;
-logic                     r_ready;
+logic                     RVALID;
+logic                     RREADY;
 
 // write address channel
-logic [ ADDR_WIDTH-1:0 ]  aw_addr;
+logic [ ADDR_WIDTH-1:0 ]  AWADDR;
 logic [            3:0 ]  aw_qos;
-logic                     aw_valid;
-logic                     aw_ready;
+logic                     AWVALID;
+logic                     AWREADY;
 
 // write data channel
-logic [ DATA_WIDTH-1:0 ]  w_data;
-logic [ STRB_WIDTH-1:0 ]  w_strb;
-logic                     w_valid;
-logic                     w_ready;
+logic [ DATA_WIDTH-1:0 ]  WDATA;
+logic [ STRB_WIDTH-1:0 ]  WSTRB;
+logic                     WVALID;
+logic                     WREADY;
 
 // write response channel
 logic [ RESP_WIDTH-1:0 ]  b_resp;
-logic                     b_valid;
+logic                     BVALID;
 logic                     b_ready;
 
-modport sp (
+modport slave (
   input ar_addr, ar_valid, ar_qos,   output ar_ready,
   input r_ready,                     output r_data, r_resp, r_valid,
   input aw_addr, aw_valid, aw_qos,   output aw_ready,
   input w_data, w_strb, w_valid,     output w_ready,
   input b_ready,                     output b_resp, b_valid
-);
-
-modport mp (
-  output ar_addr, ar_valid, ar_qos,  input ar_ready,
-  output r_ready,                    input r_data, r_resp, r_valid,
-  output aw_addr, aw_valid, aw_qos,  input aw_ready,
-  output w_data, w_strb, w_valid,    input w_ready,
-  output b_ready,                    input b_resp, b_valid
-);
-
-modport sp_read (
-  input ar_addr, ar_valid, ar_qos,   output ar_ready,
-  input r_ready,                     output r_data, r_resp, r_valid
-);
-
-modport sp_write (
-  input aw_addr, aw_valid, aw_qos,   output aw_ready,
-  input w_data, w_strb, w_valid,     output w_ready,
-  input b_ready,                     output b_resp, b_valid
-);
-
-modport mp_read (
-  output ar_addr, ar_valid, ar_qos,  input ar_ready,
-  output r_ready,                    input r_data, r_resp, r_valid
-);
-
-modport mp_write (
-  output aw_addr, aw_valid, aw_qos,  input aw_ready,
-  output w_data, w_strb, w_valid,    input w_ready,
-  output b_ready,                    input b_resp, b_valid
 );
 
 endinterface
